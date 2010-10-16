3d4eb1c68aead006cfe6f497d668a426645b6d03
bulkstream <bulkstream>
2010-10-16 17:27:04 +0000

    update version number.

--- srelay.h
+++ srelay.h
@@ -102,7 +102,7 @@ typedef    u_int32_t    socklen_t;
 # endif
 #endif
 
-#define version  "srelay 0.4.7 2009/08/30 (Tomo.M)"
+#define version  "srelay 0.4.7p4 2010/10/17 (Tomo.M)"
 
 #ifndef SYSCONFDIR
 # define SYSCONFDIR "/usr/local/etc"
